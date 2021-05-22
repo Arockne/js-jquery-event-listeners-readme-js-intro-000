3661b5c8c51bdc3684812ebe16dd88e90540496e
Daniel Calvin <arockne2003@yahoo.com>
2021-05-22 23:38:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,16 +18,16 @@ function pressIt() {
   });
 }
 
-function submitIt() {
-  $("form").on("submit", function() {
-    if ($( "input:first" ).val() === "correct") {
-      alert('Your form is going to be submitted now.');
-      return;
-    }
-    alert("you entered the wrong value");
-    return;
-  });
-}
+// function submitIt() {
+//   $("form").on("submit", function() {
+//     if ($( "input:first" ).val() === "correct") {
+//       alert('Your form is going to be submitted now.');
+//       return;
+//     }
+//     alert("you entered the wrong value");
+//     return;
+//   });
+// }
 
 $(document).ready(function(){
 // call functions here
